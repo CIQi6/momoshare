7b2bc44820d0a5aac0af83246d88e8d6bce6f182
CIQi6 <110361287+CIQi6@users.noreply.github.com>
2024-03-09 21:54:41 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -5,8 +5,7 @@
 
 
 const MMSLS = [
-    
-    "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
+   
     "https://www.maimemo.com/share/page?uid=32454734&pid=51406dd9e063b581acceb3363d60e3c3&tid=e22a83446318a25969f7f24f913fa60c",
     "https://www.maimemo.com/share/page?uid=32454734&pid=ff6a8b3a651ee2874e97b338d9ef1358&tid=e9417164027beb4e9ae0bf3d49b79ffb",
   ]
@@ -19,7 +18,7 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    " https://www.maimemo.com//share/page?uid=13840327&pid=54c96370dff75dffbfb115b8f945f5fd&tid=372558926e0cde3fd7ba28d604bf5bd4",
+    "https://www.maimemo.com/share/page?uid=13840327&pid=8a3efafeccaca698bc911f7830acd1ea&tid=ed9c742aec6beef8c9c5a80146d395f5",
     
   ]
   /**
